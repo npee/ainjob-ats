5c15a812c0eb0a971f9160f698c1849b9d903d33
npee <jmc4664@gmail.com>
2025-11-19 18:17:47 +0900

    Initial Commit

---
+++ src/main/java/com/ainjob/ats/AinjobCodingtestApplication.java
@@ -0,0 +1,13 @@
+package com.ainjob.ats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AinjobCodingtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AinjobCodingtestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ainjob/ats/AinjobCodingtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ainjob.ats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AinjobCodingtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
